a70c0c1aa2eb627c4161c909d394540642b1d6c3
Myles Bulla <dev.mbulla@gmail.com>
2023-02-16 11:00:46 -0600

    first commit

---
+++ apisee.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+help()
+{
+   # Display Help
+    clear
+    echo ""
+    echo "#########################################################################################"
+    echo -e "#            ${CYAN}Provides functions for interacting with encrypted API database  ${NC}            #"
+    echo "#########################################################################################"
+    echo ""
+    echo -e "${IGREEN}Retrieve Value from DB Syntax:${NC} apisee.sh [-t] 'Item Type' [-r] 'Application Name'"
+    echo -e "${IGREEN}Example Usage:${NC} ${BLUE}apisee.sh -t 'APIKEY' -r 'Zenoss'${NC}"
+    echo "Will retrieve value in APIKEY column for application named Zenoss"
+    echo "#########################################################################################"
+    echo ""
+    echo -e "${IGREEN}Retrieve all available data from DB:${NC} apisee.sh [-r] 'All'"
+    echo -e "${IGREEN}Example Usage:${NC} ${BLUE}apisee.sh -r 'ALL'${NC}"
+    echo "Will return table of all available data within API database"
+    echo "#########################################################################################"
+    echo ""
+    echo "Decrypt File ~ Syntax:   [-d] 'file to decrypt'"
+    echo -e "Example Usage: ${BLUE}auth.sh -d passwords.txt.enc${NC}"
+    echo "Will output contents of file to stdout"
+    echo "#########################################################################################"
+    echo ""
+}
+update()
+{
+    echo 'lol'
+}
+
+add()
+{  
+    if [[ "$ADD_VALUE" =~ ^[a-zA-Z0-9\`!@\#\$\%\^\&\*\(\)\_\+\\\{\}\[\]\;\'\"\/\.\,\<\>\|]*,[a-zA-Z0-9\`!@\#\$\%\^\&\*\(\)\_\+\\\{\}\[\]\;\'\"\/\.\,\<\>\|]*,[a-zA-Z0-9\`!@\#\$\%\^\&\*\(\)\_\+\\\{\}\[\]\;\'\"\/\.\,\<\>\|]*,.*[^,]$ ]]    
+        then
+            NAMES_ROW=$(bash auth.sh -d API_DB.enc | cut -d ',' -f 1 )
+            API_ITEM_ARRAY=(`echo $ADD_VALUE | tr ',' ' '`)
+            UNIQUE_NAME=${API_ITEM_ARRAY[0]}
+            if [[ "$NAMES_ROW" =~ "$UNIQUE_NAME" ]]
+                then echo "Error NAME is not unique. Either update the existing key or remove it and rerun"
+                else bash auth.sh -o -d 'API_DB.enc' && echo $ADD_VALUE >> 'API_DB' && bash auth.sh -e 'API_DB'
+            fi
+            #if [[ "$UNIQUE_NAME"  ]]
+        else
+            FAILED_VAR="Failed Regex Validation!"
+            printf "\n\n${RED}%85s${NC}\n" "--------------- $FAILED_VAR ---------------"
+            printf "\n\nEnsure Value Supplied Matches The Following Pattern: [ ${BLUE}%50s${NC} ]\n\n" "ExampleValue1,ExampleValue2,ExampleValue3,ExampleValue4"
+        fi
+}
+
+delete()
+{
+    NAMES_ROW=$(bash auth.sh -d API_DB.enc | cut -d ',' -f 1 )
+    API_ITEM_ARRAY=(`echo $ADD_VALUE | tr ',' ' '`)
+    UNIQUE_NAME=${API_ITEM_ARRAY[0]}
+    #if [[ "$NAMES_ROW" =~ "$UNIQUE_NAME" ]]
+
+}
+
+read()
+{
+    case "$SEARCH_TERM" in
+        All|ALL|a|A)
+            
+            printf "\n\n${CYAN}Please Maximize Your Terminal Window${NC}\n\n"
+            sleep 3
+            API_ITEMS=$(bash auth.sh -d API_DB.enc)
+            API_ITEMS_ARRAY=(`echo $API_ITEMS`)
+            COUNT=0
+            for line in "${API_ITEMS_ARRAY[@]}"
+            do
+                #echo $line
+                if (( $COUNT == 0 ))
+                    then
+                    echo " "
+                    echo $line | tr ',' ' ' | awk '{printf (" | ———————————————————— | ————————————————————————————————————————————— | ———————————————————————————————— | ———————————————————————————————— | \n")}'
+                    echo $line | tr ',' ' ' | awk '{printf (" | %-20s | %-45s | %-32s | %-32s | \n", $1, $2, $3, $4)}'
+                    echo $line | tr ',' ' ' | awk '{printf (" | ———————————————————— | ————————————————————————————————————————————— | ———————————————————————————————— | ———————————————————————————————— | \n")}'
+                else    
+                    echo $line | tr ',' ' ' | awk '{printf " | %-20s | %-45s | %-32s | %-32s | \n", $1, $2, $3, $4}'
+                fi
+                COUNT=$((COUNT + 1))
+            done
+            echo ""
+            ;;
+        *)
+            API_ITEMS=$(bash auth.sh -d API_DB.enc)
+            LINE=$(echo "${API_ITEMS}" | grep -i "$SEARCH_TERM")
+            #echo "THIS IS LINE: $LINE"
+            API_ITEM_ARRAY=(`echo $LINE | tr ',' ' '`)
+            declare -A API_HASHMAP
+            API_HASHMAP[NAME]=${API_ITEM_ARRAY[0]}
+            API_HASHMAP[APIKEY]=${API_ITEM_ARRAY[1]}
+            API_HASHMAP[SECRETKEY]=${API_ITEM_ARRAY[2]}
+            API_HASHMAP[ADDITIONALVALUE]=${API_ITEM_ARRAY[3]}
+            SELECTION=${SELECTION^^}
+            RETURN_VALUE=${API_HASHMAP[${SELECTION}]}
+            echo $RETURN_VALUE
+            ;;
+    esac
+}
+
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+IGREEN='\033[0;92m'
+
+
+while getopts "t:r:a:h" option
+do
+    case $option in
+    u)
+        update
+        ;;
+    a)
+        ADD_VALUE=${OPTARG}
+        add
+        ;;
+    d)
+        DELETION_SELECTION=${OPTARG}
+        delete
+        ;;
+    t)
+        SELECTION=${OPTARG}
+        ;;
+    r)
+        SEARCH_TERM=${OPTARG}
+        read
+        ;;
+    h)
+        help
+        ;;
+    
+    
+
+   esac
+done
+
+if (( $OPTIND == 1 )); then
+   echo "Run this tool with the -h option for details on it's usage"
+fi
---
+++ auth.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+help()
+{
+   # Display Help
+    clear
+    echo ""
+    echo "#########################################################################################"
+    echo -e "#    ${CYAN}Provides function for creating encryption keys and encrypting/decrypting files${NC}     #"
+    echo "#########################################################################################"
+    echo ""
+    echo "Create Encryption Key ~ Syntax:  [-c]"
+    echo -e "Example Usage: ${BLUE}auth.sh -c${NC}"
+    echo "Generates auth.key file in ${HOME_VAR}/.ssh/ or warn/prompt to regenerate if one exists"
+    echo "#########################################################################################"
+    echo ""
+    echo "Encrypt File ~ Syntax:   [-e] 'file to encrypt'"
+    echo -e "Example Usage: ${BLUE}auth.sh -e passwords.txt${NC}"
+    echo "Will output encrypted file of same name with .enc extension"
+    echo "#########################################################################################"
+    echo ""
+    echo "Decrypt File ~ Syntax:   [-d] 'file to decrypt'"
+    echo -e "Example Usage: ${BLUE}auth.sh -d passwords.txt.enc${NC}"
+    echo "Will output contents of file to stdout"
+    echo "#########################################################################################"
+    echo ""
+    echo "Decrypt File ~ Syntax: [-o] [-d] 'file to decrypt'"
+    echo -e "Example Usage: ${BLUE}auth.sh -o -d passwords.txt.enc${NC}"
+    echo "Will decrypt encypted file in same directory"
+    echo "#########################################################################################"
+}
+encrypt_file()
+{
+    openssl enc -aes-256-cbc -md sha512 -iter 100000 -salt -in $FILE_TO_ENCRYPT -out ${FILE_TO_ENCRYPT}.enc -pass file:$HOME_VAR/.ssh/${KEY_NAME} && \
+    rm ${SECUREAUTHPATH}/API_DB
+}
+
+decrypt_file()
+{
+    if [  -n "$O_OPTION" ]
+        then
+            FILE_NO_EXT=$(echo "${FILE_TO_DECRYPT%.*}")
+            openssl enc -aes-256-cbc -md sha512 -iter 100000 -salt -d -in ${FILE_TO_DECRYPT} -out ${FILE_NO_EXT} -pass file:$HOME/.ssh/auth.key && \
+            rm ${SECUREAUTHPATH}/API_DB.enc
+
+        else
+            openssl enc -aes-256-cbc -md sha512 -iter 100000 -salt -d -in ${FILE_TO_DECRYPT} -pass file:$HOME_VAR/.ssh/${KEY_NAME}
+    fi
+}
+
+create_key()
+{
+    FILE="${HOME}/.ssh/auth.key"
+    if test -f "$FILE"; 
+    then 
+        read -p "$FILE already exists. Are you sure you want regenerate it? - Enter Y or N: " USERINPUT
+        case "$USERINPUT" in
+        Yes|yes|Y|y)
+            echo "Regenerating authentication key..."
+            sleep 3
+            openssl rand -out "$HOME_VAR/.ssh/auth.key" 32
+            echo "A new authentication key has been generated: { $KEY_NAME } in your ${HOME}/.ssh directory"
+            exit 0
+            ;;
+        *)
+            echo "You have selected to retain your existing key..."
+            echo "Exiting"
+            exit 0
+            ;;
+        esac
+    else 
+        openssl rand -out "$HOME_VAR/.ssh/auth.key" 32
+        echo "Generated authentication key: { $KEY_NAME } in your ${HOME}/.ssh directory"
+        echo "For use with the API Auth tool, ensure all users who require access have the ${RED}same key${NC} in their own ~/.ssh/ directory"
+        exit 0
+    fi
+}
+
+############### Variable Definitions ###############
+KEY_NAME='auth.key'
+HOME_VAR=${HOME}
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+BLUE='\033[0;34m'
+
+# SECUREAUTHPATH='/bolton/bitbucket/bol-scripts/secureauth'  #   PROD
+SECUREAUTHPATH='/home/mbulla/Repos/bol-scripts/secureauth'   #   TESTING
+
+while getopts ":che:od:" option
+do
+    case $option in
+        c)
+            create_key
+            ;;
+        h)  
+            help
+            ;;
+        e)
+            FILE_TO_ENCRYPT=${OPTARG}
+            encrypt_file
+            ;;
+        o) 
+            O_OPTION=true
+            ;;
+        d) 
+            FILE_TO_DECRYPT=${OPTARG}
+            decrypt_file
+            ;;
+   esac
+done
+if (( $OPTIND == 1 )); then
+   echo "Run this tool with the -h option for details on it's usage"
+fi
+
